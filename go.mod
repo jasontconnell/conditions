91081d275c460debcf8dc24d98933b97ac7dc3f0
Jason Connell <jason.connell@herodigital.com>
2023-05-19 13:39:31 -0400

    Go 1.20

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jasontconnell/conditions
 
-go 1.14
+go 1.20
--- process.go
+++
@@ -1,9 +0,0 @@
-package conditions
-
-func Load(c string) Condition {
-	return Condition{}
-}
-
-func (c Condition) Test(t interface{}) bool {
-	return false
-}
